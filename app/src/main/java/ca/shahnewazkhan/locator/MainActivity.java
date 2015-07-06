aea8d5ed59545e43ba3adc283893e81d44699a9a
Shah <shahnewazk@gmail.com>
2015-07-05 18:57:59 -0700

    Added recyler view  & facebook callbacks

--- app/src/main/java/ca/shahnewazkhan/locator/MainActivity.java
+++ app/src/main/java/ca/shahnewazkhan/locator/MainActivity.java
@@ -2,70 +2,85 @@ package ca.shahnewazkhan.locator;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Base64;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private CallbackManager mCallbackManager;
     private Context context;
+    private ProfilePictureView profilePictureView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
 
+        setContentView(R.layout.activity_main);
+
         mCallbackManager = CallbackManager.Factory.create();
         context = getApplicationContext();
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+        profilePictureView.isInEditMode();
 
-        setContentView(R.layout.activity_main);
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.isInEditMode();
+        LoginManager.getInstance().registerCallback(mCallbackManager, mCallback);
 
-        LoginManager.getInstance().registerCallback(mCallbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                        AccessToken accessToken = loginResult.getAccessToken();
-                        Profile profile = Profile.getCurrentProfile();
+        AccessTokenTracker tracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken,
+                                                       AccessToken newToken) {
+                if(newToken == null){
+                    Toast.makeText(context, "LOGED OUT", Toast.LENGTH_SHORT).show();
+                    profilePictureView.setProfileId("");
+                }
 
-                        if(profile != null){
-                            Toast.makeText(context, profile.getName(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
+            }
+        };
 
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
     }
+
+    private FacebookCallback<LoginResult> mCallback= new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+
+            if(profile != null){
+                Toast.makeText(context, profile.getId(), Toast.LENGTH_SHORT).show();
+                Log.d("FBID", profile.getId());
+                profilePictureView.setProfileId(profile.getId());
+            }
+        }
+        @Override public void onCancel() {}
+
+        @Override public void onError(FacebookException e) {}
+    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
